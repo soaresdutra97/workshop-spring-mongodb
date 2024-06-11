1ea2cc1bb5c77bc0eab9cb5f68a0a586d7b66ba7
Chrystian Dutra <soaresdutra97@gmail.com>
2024-06-11 15:45:15 -0300

    obtendo usuario por id e exception

--- src/main/java/com/example/workshop_mongodb/services/UserService.java
+++ src/main/java/com/example/workshop_mongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.workshop_mongodb.services;
 
 import com.example.workshop_mongodb.domain.User;
 import com.example.workshop_mongodb.repository.UserRepository;
+import com.example.workshop_mongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
